f229006ac6c78030955b7b49ef2b6b396e241cba
maxbruce <bruce-lang@hotmail.com>
2016-05-19 15:35:19 +0200

    Pics + Verbindungen
    Bilder für die MainAktivity geaddet und die Verbindungen zu den anderen
    Aktivitys gemacht.

--- MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/KategorieStatistik.java
+++ MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/KategorieStatistik.java
@@ -1,13 +1,26 @@
 package com.example.mhaslehner.finanzmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class KategorieStatistik extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kategorie_statistik);
+
+
+    }
+
+    public void wochenStatistik(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),WochenStatistik.class);
+        startActivity(i);
     }
 }
--- MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.mhaslehner.finanzmanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.smiley_gruen);
+
+    }
+
+    public void addAusgabe(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),AddAusgabe.class);
+        startActivity(i);
+    }
+
+    public void addEinnahme(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),AddEinnahme.class);
+        startActivity(i);
+    }
+
+    public void kategorieStatistik(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),KategorieStatistik.class);
+        startActivity(i);
+    }
+
+    public void wochenStatistik(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),WochenStatistik.class);
+        startActivity(i);
+    }
+
+    public void verbesserungen(View view)
+    {
+        Intent i = new Intent(getApplicationContext(),Verbesserungen.class);
+        startActivity(i);
     }
 }
